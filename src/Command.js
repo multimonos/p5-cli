35e00704e04e51aa6ab494d64d5fdab8ee672224
Craig Hopgood <searaig@gmail.com>
2021-01-20 07:59:12 -0500

    better logging

--- src/Command.js
+++ src/Command.js
@@ -1,20 +1,34 @@
 import chalk from 'chalk'
+import logicon from 'log-utils'
 
 export default class Command {
 
-
-    run(config,) {
+    run(config) {
         console.log("first argument to run() is always 'config'")
         return this
     }
 
     title(title) {
-        console.log(chalk.yellow(`\n${title}:`))
+        console.log(chalk.blue(`\n${title}:`))
         return this
     }
 
-    error(err) {
-        console.log(chalk.red(`! ${err}`))
+    info(msg) {
+        console.log(` ${logicon.info} ${msg}`)
+        return this
+    }
+    error(msg) {
+        console.log(chalk.red(` ${logicon.error} ${msg}`))
+        return this
+    }
+
+    success(msg) {
+        console.log(chalk.green(` ${logicon.success} ${msg}`))
+        return this
+    }
+
+    warning(msg) {
+        console.log(chalk.yellow(` ${logicon.warning} ${msg}`))
         return this
     }
 }
