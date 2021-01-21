6a8a9dc8f81f5beef0fc7c4894b72439b6fa6ad9
Craig Hopgood <searaig@gmail.com>
2021-01-21 14:19:59 -0500

    cleanup + small spawn refactor

--- src/Command/Serve.js
+++ src/Command/Serve.js
@@ -8,7 +8,6 @@ import APP_ROOT from 'app-root-path'
 export default class ServeCommand extends Command {
 
     run(config, sketchPath) {
-        this.info(`path: ${sketchPath}`)
         const entrypoint = this.guessEntrypoint(sketchPath)
 
         if ( ! entrypoint) {
@@ -17,11 +16,13 @@ export default class ServeCommand extends Command {
             return this
         }
 
-        this.info(`entry: ${entrypoint}`)
-        this.success('building files ... ( this may take a while )')
+        const cmd = `${APP_ROOT}/node_modules/.bin/parcel ${entrypoint}` // necessary to be specific with path otherwise spawn is very very slow
 
-        // this.exec(entrypoint)
-        this.spawn(entrypoint)
+        this.info(`entry: ${entrypoint}`)
+        this.info(`cmd: ${cmd}`)
+        this.success('building files ... ( this might take a while )')
+
+        this.spawn(cmd)
 
         return this
     }
@@ -46,10 +47,8 @@ export default class ServeCommand extends Command {
         return undefined
     }
 
-    spawn(src) {
-        this.info('using spawn()')
-        const parcel = `${APP_ROOT}/node_modules/.bin/parcel` // necessary speed enhancement otherwise spawn is very very slow
-        const cmd = spawn(`${parcel} ${src}`, {
+    spawn(cmd) {
+        spawn(cmd, {
             stdio: 'inherit',
             shell: true
         })
