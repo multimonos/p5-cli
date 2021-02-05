1d1db133e153fd1451acf4fb61233f34c2438fcf
Craig Hopgood <searaig@gmail.com>
2021-02-05 11:32:39 -0500

    refactor to use @multimonos/p5-dataserver

--- src/Command/DataServer.js
+++ src/Command/DataServer.js
@@ -1,10 +1,11 @@
 
 import Command from "../Command";
+import dataserver from "@multimonos/p5-dataserver"
 
 export default class DataServerCommand extends Command {
     run(config, options) {
         console.log("options:", options)
-        require("p5-dataserver").serve(options)
+        dataserver.serve(options)
         return this
     }
 }
