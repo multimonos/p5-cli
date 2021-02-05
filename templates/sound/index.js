f9f435dd3f48c07b5dd0e7938f76ed124d24503a
Craig Hopgood <searaig@gmail.com>
2021-02-05 10:54:22 -0500

    update templates to provide window.sketch via the console in browser for easier debugging

--- templates/sound/index.js
+++ templates/sound/index.js
@@ -2,4 +2,6 @@ window.p5 = require('p5')
 
 import sketch from './sketch'
 
-new p5(sketch, document.getElementById('sketch'))
+const sketchInstance = new p5(sketch, document.getElementById('sketch'))
+
+window.sketch = sketchInstance // useful for debugging
